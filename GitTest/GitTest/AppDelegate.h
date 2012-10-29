26c9e3c36ea47d757d7ffe07788968837c833239
shidongliang <shidongliang@rocketmail.com>
2012-10-29 11:14:39 +0800

    GitTestWork
    first work on Git

---
+++ GitTest/GitTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GitTest
+//
+//  Created by MacBook Pro on 12-10-29.
+//  Copyright (c) 2012年 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GitTest/GitTest/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  GitTest
+//
+//  Created by MacBook Pro on 12-10-29.
+//  Copyright (c) 2012年 MacBook Pro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
